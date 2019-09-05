c23181dd36b52819c3e4e23533c2ce8274963139
Tran Trung Tin <trungtindev@gmail.com>
2019-09-05 16:29:18 +0700

    decaffeinate config

---
+++ bulk-decaffeinate.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  searchDirectory: './src',
+  useJSModules: true,
+  
+  fixImportsConfig: {
+    searchPath: './src',
+    absoluteImportPaths: ['./src'],
+  },
+  outputFileExtension: "ts"
+};
