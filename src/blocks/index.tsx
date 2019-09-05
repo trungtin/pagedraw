232641bd5116a522caeb41fbc1b6ff5aded0d0b1
decaffeinate <trungtindev@gmail.com>
2019-09-05 17:28:38 +0700

    decaffeinate: Run post-processing cleanups on block.coffee and 151 other files

--- src/blocks/index.tsx
+++ src/blocks/index.tsx
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 // load all blocks
 
 // regenerate with `yarn run regenerate-blocks-index`
--- src/frontend/should-subtree-render.tsx
+++ src/frontend/should-subtree-render.tsx
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 let ShouldSubtreeRender;
 import React from 'react';
 import createReactClass from 'create-react-class';
